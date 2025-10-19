5a0b0690559757a595475666e29796cf2139bc6c
LadyAmely <nawrotamelia3@gmail.com>
2025-10-20 00:50:31 +0200

    feat: add error response

--- src/main/java/com/example/market/dto/response/ErrorResponse.java
+++ src/main/java/com/example/market/dto/response/ErrorResponse.java
@@ -1,4 +1,15 @@
 package com.example.market.dto.response;
 
-public record ErrorResponse() {
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.OffsetDateTime;
+
+public record ErrorResponse(
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        OffsetDateTime timestamp,
+        int status,
+        String errorMessage,
+        String message,
+        String path
+) {
 }
